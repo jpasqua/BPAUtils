8cdb68ff72bcc09bf39964d928b54e7be1a9cbfe
Joe Pasqua <joe@noroomattheinn.org>
2021-11-02 11:27:59 -0700

    Add countof template function for array sizes

--- src/BPABasics.h
+++ src/BPABasics.h
@@ -13,6 +13,9 @@
 #define FREE(p) { if (p) free(p); }
 #define ARRAY_SIZE(a) (sizeof(a)/sizeof((a)[0]))
 
+template<std::size_t N, class T>
+constexpr std::size_t countof(T(&)[N]) { return N; }
+
 namespace Basics {
   // ----- Basic Types
   using Pin = uint8_t;
