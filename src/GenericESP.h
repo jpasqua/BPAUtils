a8f4fe656ab5c2d1541c2ebcdd3b86172c174038
Joe Pasqua <joe@noroomattheinn.org>
2023-04-22 13:48:53 -0700

    Add a function that returns the ADC max val

--- src/GenericESP.h
+++ src/GenericESP.h
@@ -13,6 +13,14 @@ namespace GenericESP {
   uint32_t getFreeHeap();
   uint8_t getHeapFragmentation();
   uint16_t getMaxFreeBlockSize();
+
+#if defined(ESP8266)
+  constexpr uint16_t getADCMax() { return 1023; }
+#elif defined(ESP32)
+  constexpr uint16_t getADCMax() { return 4095; }
+#else
+  #error "Must be an ESP8266 or ESP32"
+#endif  
 }
 
 #endif // GenericESP_h
